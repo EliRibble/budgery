21e46dfb2c3fe4ad57f58b8bafb8f3002f8e5050
Eli Ribble <eli@theribbles.org>
2021-05-12 08:14:14 -0600

    Rework config module, add requests to get well-known config endpoint.
    This is all geared toward my usage of Keycloak in my environment.
    I'm probably doing this wrong.

--- budgery/__init__.py
+++ budgery/__init__.py
@@ -1,20 +1,30 @@
+import logging
 import os
 from quart import Quart
+import sys
 
 import budgery.config
 import budgery.routes
 
-def create_app(mode="Development") -> Quart:
+def create_app() -> Quart:
 	"Create the quart app."
+	config = budgery.config.load()
 	app = Quart("budgery")
-	app.config.from_object(f"budgery.config.{mode}")
-	if os.environ.get("BUDGERY_SETTINGS"):
-		app.config.from_envvar('BUDGERY_SETTINGS')
+	app.config.from_mapping(config)
+		
 	app.register_blueprint(
 		budgery.routes.blueprint
 	)
 	return app
 
-if __name__ == "__main__":
-	app = create_app()
+def main() -> None:
+	logging.basicConfig(level=logging.DEBUG)
+	try:
+		app = create_app()
+	except budgery.config.ConfigurationError as ex:
+		print(ex)
+		sys.exit(1)
 	app.run(port=10100)
+
+if __name__ == "__main__":
+	main()
--- budgery/config.py
+++ budgery/config.py
@@ -1,10 +1,48 @@
-class Config:
-	DEBUG = False
-	TESTING = False
-	SECRET_KEY = 'secret'
+import os
+import toml
+from typing import Dict, Union
 
-class Development(Config):
-	DEBUG = True
+import requests
 
-class Production(Config):
-	SECRET_KEY = 'a different secret key'
+DEFAULT_CONFIG_FILE = "/etc/budgery.toml"
+
+DEFAULT_CONFIG = {
+	"DEBUG": True,
+	"SECRET_KEY": "replace me with something good",
+	"SSO_CLIENT_ID": None,
+	"SSO_NAME": None,
+	"SSO_OIDC_WELL_KNOWN_URL": None,
+	"TESTING": False,
+}
+
+Config = Dict[str, Union[bool, int, None, str]]
+
+class ConfigurationError(Exception):
+	pass
+
+def load() -> Config:
+	"Load up the configuration"
+	settings_file = os.environ.get("BUDGERY_SETTINGS", DEFAULT_CONFIG_FILE)
+	config = DEFAULT_CONFIG.copy()
+	try:
+		with open(settings_file, "r") as c:
+			from_file = toml.load(c)
+			config.update(from_file)
+			config.update(_get_oidc(config))
+			rut
+	except OSError:
+		pass
+	return config
+
+def _get_oidc(config: Config) -> Config:
+	"Download OpenID Connect information."
+	url = config["SSO_OIDC_WELL_KNOWN_URL"]
+	if not url:
+		return {}
+	response = requests.get(url)
+	if not response.ok:
+		raise ConfigurationError(f"Unable to get well-known configuration from {url}: {response.status_code} {response.text}")
+	data = response.json()
+	return {
+		'SSO_OIDC_AUTHORIZATION_ENDPOINT': data["authorization_endpoint"],
+	}
