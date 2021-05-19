52a0875d05a2d9f4e47068289adda068006f4052
Eli Ribble <eli@theribbles.org>
2021-05-19 10:11:23 -0600

    Clean up old Quart-based logic.
    I'm convinced that I will like FastAPI *much* better now.
    
    And I've already reproduced (and more) what the Quart stuff could
    do. And did it faster. And better.

--- budgery/routes.py
+++
@@ -1,8 +0,0 @@
-from quart import Blueprint, render_template
-
-blueprint = Blueprint("all", __name__)
-
-@blueprint.route("/")
-async def hello() -> None:
-	return await render_template("index.html")
-
