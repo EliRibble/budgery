22ef37260966a16b40eb7d7c8adb84ae4ca352fd
Eli Ribble <eli@theribbles.org>
2022-05-03 12:05:51 -0600

    Fix BudgetEntry.created default.
    The original was timezone-aware which doesn't work because SQLite can't parse
    it...and we don't care anyway.

--- budgery/db/alembic/versions/a248e26bd2bb_add_budgetentry_created.py
+++ budgery/db/alembic/versions/a248e26bd2bb_add_budgetentry_created.py
@@ -5,7 +5,6 @@ Revises: 59a9c3445a9b
 Create Date: 2022-05-03 11:51:33.351141
 
 """
-import datetime
 from alembic import op
 import sqlalchemy as sa
 
@@ -19,16 +18,11 @@ depends_on = None
 
 def upgrade():
 	op.add_column('budget_entry', sa.Column('created', sa.DateTime(), nullable=True))
-	op.add_column('budget_entry_history', sa.Column('created', sa.DateTime(), autoincrement=False, nullable=True, server_default=datetime.datetime(
-		year=2000,
-		month=1,
-		day=1,
-		hour=0,
-		minute=0,
-		second=0,
-	).isoformat()))
+	op.add_column('budget_entry_history', sa.Column('created', sa.DateTime(), autoincrement=False, nullable=True, server_default="2000-01-01 00:00:00"))
 
 
 def downgrade():
-	op.drop_column('budget_entry_history', 'created')
-	op.drop_column('budget_entry', 'created')
+	with op.batch_alter_table("budget_entry_history", schema=None) as batch_op:
+		batch_op.drop_column('created')
+	with op.batch_alter_table("budget_entry", schema=None) as batch_op:
+		batch_op.drop_column('created')
