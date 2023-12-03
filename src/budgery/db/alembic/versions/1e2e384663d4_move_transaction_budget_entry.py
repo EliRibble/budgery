c3fe2d63c5899192c324fa65bd49095aee3112b2
Eli Ribble <eli@theribbles.org>
2023-12-02 21:38:12 -0700

    Fix my column rename of Transaction.budget_entry_id
    The previous version didn't actually work on SQLite

--- src/budgery/db/alembic/versions/1e2e384663d4_move_transaction_budget_entry.py
+++ src/budgery/db/alembic/versions/1e2e384663d4_move_transaction_budget_entry.py
@@ -17,22 +17,10 @@ depends_on = None
 
 
 def upgrade():
-    with op.batch_alter_table('budget_entry_history', schema=None) as batch_op:
-        batch_op.create_foreign_key(None, 'budget', ['budget_id'], ['id'])
-
     with op.batch_alter_table('transaction', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('budget_entry_id', sa.Integer(), nullable=True))
-        batch_op.drop_constraint('fk_budget_entry_id', type_='foreignkey')
-        batch_op.create_foreign_key('fk_budget_entry_id', 'budget_entry', ['budget_entry_id'], ['id'])
-        batch_op.drop_column('budget_entry')
+        batch_op.alter_column('budget_entry', new_column_name="budget_entry_id")
 
 
 def downgrade():
     with op.batch_alter_table('transaction', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('budget_entry', sa.INTEGER(), nullable=True))
-        batch_op.drop_constraint('fk_budget_entry_id', type_='foreignkey')
-        batch_op.create_foreign_key('fk_budget_entry_id', 'budget_entry', ['budget_entry'], ['id'])
-        batch_op.drop_column('budget_entry_id')
-
-    with op.batch_alter_table('budget_entry_history', schema=None) as batch_op:
-        batch_op.drop_constraint(None, type_='foreignkey')
+        batch_op.alter_column('budget_entry_id', new_column_name="budget_entry")
