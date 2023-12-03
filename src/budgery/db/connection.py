5382618acc1c5e33690be36772a5997d3f5981db
Eli Ribble <eli@theribbles.org>
2023-12-03 11:14:55 -0700

    Log the database connection URL.
    Useful for ensuring tests are connecting to the test database

--- src/budgery/db/connection.py
+++ src/budgery/db/connection.py
@@ -1,3 +1,4 @@
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
@@ -5,9 +6,13 @@ from starlette.config import Config
 
 from budgery.db.history_meta import versioned_session
 
+LOGGER = logging.getLogger(__name__)
+
 def connect(config: Config):
+	url = config("SQL_ALCHEMY_DATABASE_URL", default="sqlite:///./budgery.db")
+	LOGGER.info("Connecting to DB at %s", url)
 	return create_engine(
-		config("SQL_ALCHEMY_DATABASE_URL", default="sqlite:///./budgery.db"),
+		url,
 		connect_args={"check_same_thread": False},
 	)
 
