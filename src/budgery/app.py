2c6274f4ee45da870be8e5b0a38b28c8c238371b
Eli Ribble <eli@theribbles.org>
2023-12-03 11:14:16 -0700

    Remove on_event from app configuration.
    They weren't actually doing anything and that API is deprecated from
    FastAPI in favor of lifetimes.

--- src/budgery/app.py
+++ src/budgery/app.py
@@ -1,19 +1,8 @@
 "Module for the FastAPI application."
+from contextlib import asynccontextmanager
 import logging
 
 from budgery.db import connection as db_connection
 from budgery.main import app
 
-LOGGER = logging.getLogger(__name__)
-
 logging.basicConfig(level=logging.INFO)
-@app.on_event("startup")
-def startup() -> None:
-	LOGGER.info("Startup for main app.")
-	#apply_migrations(str(BaseMeta.database.url))
-	#if not BaseMeta.database.is_connected:
-		#await BaseMeta.database.connect()
-
-@app.on_event("shutdown")
-def shutdown() -> None:
-	LOGGER.info("Shutdown for main app.")
